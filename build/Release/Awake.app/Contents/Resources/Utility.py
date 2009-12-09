90afc95910790537fb2f62f7c7f77e7ed12969bf
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2009-12-09 17:10:41 -0500

    Added Copyleft and now can detect new versions

--- build/Release/Awake.app/Contents/Resources/Utility.py
+++
@@ -1,8 +0,0 @@
-from Foundation import *
-from AppKit import *
-import objc
-from PyObjCTools import AppHelper
-
-def runAppScriptCommand(s):
-        ap = NSAppleScript.alloc().initWithSource_(s)
-        ap.executeAndReturnError_(None)
--- helper/Utility.py
+++ helper/Utility.py
@@ -1,3 +1,19 @@
+##    WakeMeUp - Highly Configurable Alarm Clock for OS X
+##    Copyright (C) 2009, Saptarshi Guha, saptarshi.guha@gmail.com
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from Foundation import *
 import logging
 from LogWindowController import *
@@ -8,7 +24,7 @@ from PyObjCTools import AppHelper
 import time
 import sys
 import copy
-
+VERSION_ID = 1.0
 HOOKS={'ONAWAKE':"on_awake"
        ,"ONQUIT":"on_quit"
        ,"ONRELOAD":"on_reload"
@@ -49,7 +65,22 @@ class AwakeLog(NSObject):
 		AwakeLog.logcontroller = logger
 		AwakeLog.logcontroller.clear_(None)
 		AwakeLog.logcontroller.switchOnScroll()
+		self.performSelector_withObject_afterDelay_("checkForNewVersion",None,1)
 		return self
+	def checkForNewVersion(self):
+		import urllib2
+		# try:
+		response = urllib2.urlopen('http://www.stat.purdue.edu/~sguha/dn/wakemeup.version')
+		package = response.read()
+		dd={}
+		exec package in dd
+		dd=dd['awake_news']
+		if dd['version']> VERSION_ID:
+			dwnurl = dd['download']
+			AwakeLog.logcontroller.statusbarmessage = 'New Version: '+dwnurl + "\nInfo:"+dd.get('news')[:40]
+		# except :
+			# pass
+
 	def info(self, message):
 		self.log("info",str(message))
 		
