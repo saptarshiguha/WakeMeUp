6deb5e56ee5a564adc96469d18651db4edaa0b31
Saptarshi Guha <saptarshi.guha@gmail.com>
2011-03-30 02:22:45 -0700

    massive changes

---
+++ mailgen.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+import time,os,email,re
+## We are just going to look at emails in last 1 minute which would
+## roughly correspond to recent emails ideally i would record the time
+## in pre-sync and post-sync and find emails in between
+SERVERPORT = 8080
+MAILDIR = ("/Users/yanger/MyMail/.songrequest/cur",)#,"/Users/sguha/MozMail/INBOX/new/")
+TIMEWINDOW = 60
+def checkValidTo(msg):
+    return msg['To'] == "saptarshi.guha+play@gmail.com" and msg['From']=="saptarshi.guha@gmail.com"
+
+def mailViaGmail(to, subject, text,attach=None,gmailuser=os.environ["GMAIL_USER"], gmailpwd=os.environ["GMAIL_PASSWORD"]):
+    try:
+        import smtplib
+        from email.MIMEMultipart import MIMEMultipart
+        from email.MIMEBase import MIMEBase
+        from email.MIMEText import MIMEText
+        from email import Encoders
+        import os,glob,sys
+        msg = MIMEMultipart()
+        msg['From'] = gmailuser
+        msg['To'] = to
+        msg['Subject'] = subject
+        msg.attach(MIMEText(text))
+        mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmailuser, gmailpwd)
+        mailServer.sendmail(gmailuser, to, msg.as_string())
+        mailServer.close()
+        return True
+    except:
+        import sys
+        sys.stderr.write("GMAIL ERROR:\n")
+        import traceback
+        traceback.print_exc()
+        return False
+
+## if subject is info, just reply with conf file
+## First line is music e.g. 'KCRW...' no quotes, can be preceded with a 'play'
+## after that (if quotes, must be first word on sentence):
+## time ... e.g. in 5 minutes, at 7:00 pm today
+## {'stop','dur','duration'} and then a time  to stop at time
+## {minvol,volmin,min}[SPACE]#
+## {maxvol, volmax, max}[SPACE]#
+## {fadein}[SPACE]#
+
+def read_body(subject,lines):
+    try:
+        results = {'error':False,'info':False,'max':80,'min':0,'fadein':20*60,'when':'in 30 minutes','stop':False}
+        if subject.lower().find("info")>=0 or subject.lower().find("help")>=0:
+            results['info']=True
+            return results
+        if subject.lower().find("stop")>=0 or subject.lower().find("kill")>=0:
+            results['stop'] = True
+            return results
+        results['natural'] = lines[0].strip()
+        for c in lines[1:]:
+            if c.startswith("#"):
+                next
+            c = c.strip()
+            if c.startswith("maxvol") or c.startswith("volmax") or c.startswith("max"):
+                results['max'] = str(re.split(" ",c,1)[1])
+            elif c.startswith("minvol") or c.startswith("volmin") or c.startswith("min"):
+                        results['min'] = str(re.split(" ",c,1)[1])
+            elif c.startswith("fade"):
+                results['fadein'] = str(eval(re.split(" ",c,1)[1]))
+            elif c.startswith("stop") or c.startswith("dur"):
+                results['dur'] = str(eval(re.split(" ",c,1)[1]))
+            else:
+                pass
+    except Exception:
+        import traceback
+        return {'error':True,'errordoc':traceback.format_exc()}
+    return results
+
+def getData(sock):
+    data = sock.recv(1024)
+    string = ""
+    while len(data):
+        string = string + data
+        data = sock.recv(1024)
+    return string
+def send_message_to_server(result):
+    import socket,sys
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('127.0.0.1', SERVERPORT))
+    if result['info']:
+        sock.send('info')
+        string = getData(sock)
+        sock.close()
+        return string
+    elif result['stop']:
+        sock.send('stop')
+        string = getData(sock)
+        sock.close()
+        return string
+    else:
+        x="play %(natural)s\n%(min)s\n%(max)s\n%(fadein)s\n" % result
+        if result.get('dur',None)!=None:
+            x="%s%s" % (x, result['dur'])
+        sock.send(x)
+        string = getData(sock)
+        sock.close()
+        return string
+def main():
+    new_dir = MAILDIR
+    recent_window = TIMEWINDOW
+    curr_time = time.time()
+    old=set([])
+    for n in new_dir:
+        recent_emails = set.union(old,set([ n+x for x in os.listdir(n) if (curr_time- os.path.getctime(n+x)) < recent_window]))
+    recent_emails = list(recent_emails)
+    for x in recent_emails:
+## Scan emails to see which one is to saptarshi.guha+playmusic@gmail.com
+        x=open(x,'r')
+        p=email.Parser.Parser()
+        msg=p.parse(x)
+        to_address = email.Utils.parseaddr(msg['To'])[1]
+        subject = msg['Subject']
+        if checkValidTo(msg):
+            body= msg.get_payload()
+            result = read_body(subject, body.split("\n"))
+            if not result['error']:
+                res,resp = send_message_to_server(result).split(" ",1)
+                if msg['Reply-To']:
+                    to = msg['Reply-To']
+                else:
+                    to = msg['From']
+                mailViaGmail(to,"Play-Info:%s" % res,resp)
+            else:
+                mailViaGmail(to,"Play-Info:Error",result['errordoc'])
+
+
+if __name__ == "__main__":
+    main()
---
+++ webserver.py
@@ -0,0 +1,59 @@
+#Copyright Jon Berg , turtlemeat.com
+from Foundation import *
+from AppKit import *
+from Administration import Administration
+import objc
+objc.setVerbose(True)
+import sys
+import string,cgi,time
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import traceback
+from Utility import *
+from socket import *      #im
+
+class MyServer:
+    def __init__(self, conffile, port,delegate):
+        self.serv = socket( AF_INET,SOCK_STREAM)
+        self.port = port
+        self.serv.bind((('',self.port)))
+        self.appdelegate = delegate
+        self.awakelog = AwakeLog.alloc().initWithInfo_( {'filename':__name__})
+    def startnow(self):
+        self.awakelog.info("Will listen on %s" % str(self.port))
+        while True:
+            self.serv.listen(5)
+            conn,addr = self.serv.accept()
+            self.awakelog.info("Got a connection from %s" % str((addr)))
+            request = conn.recv(1024*5).strip()
+            if request == "info":
+                self.awakelog.info("User wanted 'info'")
+                conn.send("""OK 
+## if subject is info/help, just reply with conf file
+## First line is music e.g. 'play ['KCRW...'  as in natural parse
+## {'stop','dur','duration'} and then a time  to stop at time
+## {minvol,volmin,min}[SPACE]#
+## {maxvol, volmax, max}[SPACE]#
+## {fadein}[SPACE]# """ +"\n\n" + open(self.appdelegate.cfgfile,"r").read()+"\n")
+            elif request.startswith("play"):
+                pr = request.split(" ",1)[1]
+                self.awakelog.info("User wanted to 'play' : %s" %pr)
+                result =self.appdelegate.run_server_play(pr)
+                conn.send(result)
+            elif request.startswith("stop"):
+                result = self.appdelegate.stop_server_play()
+                conn.send(result)
+            else:
+                conn.send("NOTOK Please don't send me rubbish: %s" % request)
+            conn.close()
+            self.awakelog.info("Processed Remote User Input")
+class AwakeServer(NSObject):
+    def initWithInfo_(self, info):
+        self = super(AwakeServer, self).init()
+        if self is None: return None
+        self.server = MyServer(info[0],int(info[1]),info[2])
+        return self
+    def startnow(self):
+        self.pool=NSAutoreleasePool.alloc().init()
+        self.server.startnow()
+
